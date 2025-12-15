05c4242df33901ec370b184ade60b81fe393504a
Subhash J <145677826+Subhash23-09@users.noreply.github.com>
2025-12-15 16:36:51 +0530

    Delete healthbackend directory

--- healthbackend/utils/exceptions.py
+++
@@ -1,8 +0,0 @@
-class AuthError(Exception):
-    pass
-
-class AgentError(Exception):
-    pass
-
-class InputError(Exception):
-    pass
--- healthbackend/wsgi.py
+++
@@ -1,4 +0,0 @@
-from healthbackend.app import app
-
-if __name__ == "__main__":
-    app.run()
